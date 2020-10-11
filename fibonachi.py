ce72ea818598326f6f09bbe0a09a77a8869e96f8
Moein Rezaie <72414710+mrgmcs@users.noreply.github.com>
2020-10-11 17:46:43 +0330

    Add files via upload

---
+++ fibonachi.py
@@ -0,0 +1,23 @@
+n=int(input("Enter max number"))
+
+firstnum=1
+secondnum=2
+
+m=list(1)#this sequence has two "1" , and we will append other one in next loop
+
+for i in range(1,n+1):
+    #find next num, "n" times!
+    newnum=firstnum+secondnum
+    #find new number that is sum of first and second numbers
+    m.append(firstnum)
+    #add numbers to list
+    firstnum=secondnum
+    #new "firts number" 
+    secondnum=newnum
+    #new "second number"
+    
+print(m)    
+
+#wrote by Moein Rezaie
+#github: mrgmcs
+#instagram: mrg.mcs
